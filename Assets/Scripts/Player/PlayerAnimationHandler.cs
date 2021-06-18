ef4a2f0d72b6827d46ba89ec8fc8e95f81658e33
Roman <Roman-graf2000@gmx.at>
2021-06-18 21:50:40 +0200

    Add Animations + Controller

--- Assets/Scripts/Player/PlayerAnimationHandler.cs
+++ Assets/Scripts/Player/PlayerAnimationHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TwoD_Animationcontroller : MonoBehaviour
+public class PlayerAnimationHandler : MonoBehaviour
 {
 
     Animator animator;
